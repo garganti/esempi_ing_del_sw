89889d0f19cffd89e04d0d5aa1cc32d8ee8d0065
Angelo Gargantini <angelo.gargantini@unibg.it>
2025-12-04 11:02:03 +0100

    vaadin

---
+++ codice/applicazione_vaadin_nosb/src/main/java/applicazione_vaadin_nosb/GreetService.java
@@ -0,0 +1,12 @@
+package applicazione_vaadin_nosb;
+
+public class GreetService {
+
+    public String greet(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello anonymous user";
+        } else {
+            return "Hello " + name;
+        }
+    }
+}
